93ab15221a9cac6a58c1f6af5fac45e06f83dcd8
Mikey-mw <mikeywubshet@gmail.com>
2023-12-13 14:31:46 -0800

    5

--- _atoi.c
+++ _atoi.c
@@ -13,51 +13,53 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks if character is a delimeter
- * @ch: the char to check
- * @delim_str: the delimeter string
+ * @c: the char to check
+ * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char ch, char *delim_str)
+int is_delim(char c, char *delim)
 {
-	while (*delim_str)
-		if (*delim_str++ == ch)
+	while (*delim)
+		if (*delim++ == c)
 			return (1);
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @input: The character to input
- * Return: 1 if char_input is alphabetic, 0 otherwise
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
-int _isalpha(int input)
+
+int _isalpha(int c)
 {
-	if ((input >= 'a' && input <= 'z') || (input >= 'A' && input <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
 /**
- * _atoi - converts a string to an integer
- * @str: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
-int _atoi(char *str)
+
+int _atoi(char *s)
 {
-	int index, sign = 1, flag = 0, output;
+	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (index = 0;  str[index] != '\0' && flag != 2; index++)
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
-		if (str[index] == '-')
+		if (s[i] == '-')
 			sign *= -1;
 
-		if (str[index] >= '0' && str[index] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (str[index] - '0');
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
